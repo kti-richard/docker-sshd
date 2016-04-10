df0afc3288522df718add2fb581fa1525d7c6482
Evan Sosenko <razorx@evansosenko.com>
2016-04-10 11:30:26 -0700

    Always 2 spaces for indent in entry.sh

--- entry.sh
+++ entry.sh
@@ -8,21 +8,21 @@ DAEMON=sshd
 
 # Copy default config from cache
 if [ ! "$(ls -A /etc/ssh)" ]; then
-   cp -a /etc/ssh.cache/* /etc/ssh/
+ cp -a /etc/ssh.cache/* /etc/ssh/
 fi
 
 # Generate Host keys, if required
 if ! ls /etc/ssh/ssh_host_* 1> /dev/null 2>&1; then
-    ssh-keygen -A
+  ssh-keygen -A
 fi
 
 # Fix permissions, if writable
 if [ -w ~/.ssh ]; then
-    chown root:root ~/.ssh && chmod 700 ~/.ssh/
+  chown root:root ~/.ssh && chmod 700 ~/.ssh/
 fi
 if [ -w ~/.ssh/authorized_keys ]; then
-    chown root:root ~/.ssh/authorized_keys
-    chmod 600 ~/.ssh/authorized_keys
+  chown root:root ~/.ssh/authorized_keys
+  chmod 600 ~/.ssh/authorized_keys
 fi
 
 # Warn if no config
@@ -31,24 +31,24 @@ if [ ! -e ~/.ssh/authorized_keys ]; then
 fi
 
 stop() {
-    echo "Received SIGINT or SIGTERM. Shutting down $DAEMON"
-    # Get PID
-    pid=$(cat /var/run/$DAEMON/$DAEMON.pid)
-    # Set TERM
-    kill -SIGTERM "${pid}"
-    # Wait for exit
-    wait "${pid}"
-    # All done.
-    echo "Done."
+  echo "Received SIGINT or SIGTERM. Shutting down $DAEMON"
+  # Get PID
+  pid=$(cat /var/run/$DAEMON/$DAEMON.pid)
+  # Set TERM
+  kill -SIGTERM "${pid}"
+  # Wait for exit
+  wait "${pid}"
+  # All done.
+  echo "Done."
 }
 
 echo "Running $@"
 if [ "$(basename $1)" == "$DAEMON" ]; then
-    trap stop SIGINT SIGTERM
-    $@ &
-    pid="$!"
-    mkdir -p /var/run/$DAEMON && echo "${pid}" > /var/run/$DAEMON/$DAEMON.pid
-    wait "${pid}" && exit $?
+  trap stop SIGINT SIGTERM
+  $@ &
+  pid="$!"
+  mkdir -p /var/run/$DAEMON && echo "${pid}" > /var/run/$DAEMON/$DAEMON.pid
+  wait "${pid}" && exit $?
 else
-    exec "$@"
+  exec "$@"
 fi
